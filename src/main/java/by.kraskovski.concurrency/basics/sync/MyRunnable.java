acdece707a65b4035101b45f4de9973bcebafd85
akraskovski <artem.kraskovski@gmail.com>
2017-10-15 13:20:03 +0300

    synchronized more detail example

--- src/main/java/by.kraskovski.concurrency/basics/sync/MyRunnable.java
+++ src/main/java/by.kraskovski.concurrency/basics/sync/MyRunnable.java
@@ -1,18 +1,18 @@
 package by.kraskovski.concurrency.basics.sync;
 
 public class MyRunnable implements Runnable {
-    private final SynchronizedExample synchronizedExample;
+    private final CommonResource commonResource;
 
-    public MyRunnable(final SynchronizedExample synchronizedExample) {
-        this.synchronizedExample = synchronizedExample;
+    public MyRunnable(final CommonResource commonResource) {
+        this.commonResource = commonResource;
     }
 
     @Override
     public void run() {
-        synchronized (synchronizedExample) {
+        synchronized (commonResource) {
             for (int count = 0; count < 5; count++) {
-                synchronizedExample.increment();
-                System.out.printf("Thread: %s. Counter: %d%n", Thread.currentThread().getName(), synchronizedExample.getCounter());
+                commonResource.increment();
+                System.out.printf("Thread: %s. Counter: %d%n", Thread.currentThread().getName(), commonResource.getCounter());
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException ignored) {
--- src/main/java/by.kraskovski.concurrency/basics/sync/SynchronizedExample.java
+++ src/main/java/by.kraskovski.concurrency/basics/sync/SynchronizedExample.java
@@ -1,20 +1,11 @@
 package by.kraskovski.concurrency.basics.sync;
 
 public class SynchronizedExample {
-    private int counter = 0;
-
-    public void increment() {
-        counter++;
-    }
-
-    public int getCounter() {
-        return counter;
-    }
 
     public static void main(final String... args) {
-        final SynchronizedExample synchronizedExample = new SynchronizedExample();
+        final CommonResource commonResource = new CommonResource();
         for (int count = 0; count < 5; count++) {
-            new Thread(new MyRunnable(synchronizedExample)).start();
+            new Thread(new MyRunnable(commonResource)).start();
         }
     }
 }
