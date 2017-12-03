56413c272ce01f285ee464ee457879f1606d138c
akraskovski <artem.kraskovski@gmail.com>
2017-12-03 19:51:18 +0300

    added schedule executors example

---
+++ src/main/java/by.kraskovski.concurrency/concurrency/schedule/ScheduleExample.java
@@ -0,0 +1,30 @@
+package by.kraskovski.concurrency.concurrency.schedule;
+
+import by.kraskovski.concurrency.concurrency.callable.MyCallable;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Example of usage {@link java.util.concurrent.ScheduledExecutorService}.
+ */
+public class ScheduleExample {
+
+    public static void main(String[] args) {
+        final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        final FutureTask<String> futureTask = new FutureTask<>(new MyCallable());
+
+        scheduledExecutorService.schedule(futureTask, 5, TimeUnit.SECONDS);
+
+        try {
+            System.out.printf("Thread: %s has finished.%n", futureTask.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            scheduledExecutorService.shutdown();
+        }
+    }
+}
