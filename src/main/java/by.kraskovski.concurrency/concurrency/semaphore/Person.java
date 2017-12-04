fc3264528f672bb3f7a173fb7c4011618e855b43
akraskovski <artem.kraskovski@gmail.com>
2017-12-04 20:50:38 +0300

    added semaphore example

---
+++ src/main/java/by.kraskovski.concurrency/concurrency/semaphore/Person.java
@@ -0,0 +1,28 @@
+package by.kraskovski.concurrency.concurrency.semaphore;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * Custom {@link Thread} implementation with person queue in store.
+ */
+public class Person extends Thread {
+
+    private final Semaphore cashbox;
+
+    Person(final Semaphore cashbox) {
+        this.cashbox = cashbox;
+    }
+
+    @Override
+    public void run() {
+        System.out.printf("%s waiting for order in shop%n", this.getName());
+        try {
+            cashbox.acquire();
+            System.out.println(this.getName() + " buying products");
+            Thread.sleep(2000);
+            cashbox.release();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/by.kraskovski.concurrency/concurrency/semaphore/SemaphoreExample.java
@@ -0,0 +1,18 @@
+package by.kraskovski.concurrency.concurrency.semaphore;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * Example of usage {@link java.util.concurrent.Semaphore}.
+ */
+public class SemaphoreExample {
+
+    public static void main(final String... args) {
+        final int maxPermits = 2;
+        final Semaphore cashbox = new Semaphore(maxPermits);
+
+        for (int counter = 0; counter < 10; counter++) {
+            new Person(cashbox).start();
+        }
+    }
+}
